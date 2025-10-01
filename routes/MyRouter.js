6ab6bed2852f0d3143f7c5b3b8c72e3e5444af36
weerapat-suw <msmark0015@gmail.com>
2025-10-01 23:59:45 +0700

    add register

--- routes/MyRouter.js
+++ routes/MyRouter.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const router = express.Router()
-
+router.get("/no2",(req,res)=>{
+    res.render("register.ejs")
+})
 router.get("/",(req,res)=>{
     res.render("login.ejs")
 })
 router.get("/home",(req,res)=>{
     res.render("home.ejs")
 })
+
 // router.get("/",(req,res)=>{
 //     res.sendFile(path.join(__dirname,"../login.html"))
 // })
