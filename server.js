ff59cec15b4ab850c273590f8f42fc61293dcab7
Phat <163808396+Phatlaksanai@users.noreply.github.com>
2025-10-01 21:28:40 +0700

    แก้ชื่ออีกรอบ

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 
 app.set('views',path.join(__dirname,'views'))
 app.set('view engine','ejs')
-app.use(router)
 app.use(express.static(path.join(__dirname,'public')))
+app.use(router)
 
 app.listen(8080,()=>{
     console.log("Run Server port 8080 http://localhost:8080")
