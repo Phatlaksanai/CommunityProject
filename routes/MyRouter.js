fefd6032fe5c232a6aac27a77dde02b124a7196e
Phat <163808396+Phatlaksanai@users.noreply.github.com>
2025-10-02 12:13:06 +0700

    เพิ่ม db

--- routes/MyRouter.js
+++ routes/MyRouter.js
@@ -1,28 +1,17 @@
 const express = require('express')
 const router = express.Router()
-router.get("/no2",(req,res)=>{
-    res.render("register.ejs")
-})
+
 router.get("/",(req,res)=>{
-    res.render("login.ejs")
-})
-router.get("/home",(req,res)=>{
     res.render("home.ejs")
 })
-
-// router.get("/",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"../login.html"))
-// })
-// router.get("/home",(req,res)=>{
-//     res.status(200)
-//     res.type('text/html')
-//     res.sendFile(path.join(__dirname,"../index.html"))
-// })
-// router.get("/upload",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"../upload.html"))
-// })
-// router.get("/report",(req,res)=>{
-//     res.sendFile(path.join(__dirname,"../report.html"))
-// })
+router.get("/login",(req,res)=>{
+    res.render("login.ejs")
+})
+router.get("/register",(req,res)=>{
+    res.render("register.ejs")
+})
+router.post("/regiter",(req,res)=>{
+    console.log("regiter.ejs")
+})
 
 module.exports = router
--- server.js
+++ server.js
@@ -2,12 +2,27 @@ const express = require('express')
 const path = require('path')
 const router = require('./routes/MyRouter.js')
 const app = express()
+const mysql = require('mysql2');
 
-app.set('views',path.join(__dirname,'views'))
-app.set('view engine','ejs')
-app.use(express.static(path.join(__dirname,'public')))
+const connection = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'Fatcat14.exe',
+    database: 'community'
+});
+
+connection.connect((err) => {
+    if (err) {
+        console.error('not conect:', err);
+        return;
+    }
+    console.log('connecting!');
+});
+app.set('views', path.join(__dirname, 'views'))
+app.set('view engine', 'ejs')
+app.use(express.static(path.join(__dirname, 'public')))
 app.use(router)
 
-app.listen(8080,()=>{
-    console.log("Run Server port 8080 http://localhost:8080")
+app.listen(8080, () => {
+    console.log("http://localhost:8080")
 })
