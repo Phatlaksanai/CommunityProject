6b1fcd1eae2f8b2c21bcace75fde42a80f7d0b99
Phat <163808396+Phatlaksanai@users.noreply.github.com>
2025-10-03 16:57:10 +0700

    เพิ่ม .env

--- server.js
+++ server.js
@@ -3,25 +3,29 @@ const path = require('path')
 const router = require('./routes/MyRouter.js')
 const app = express()
 const mysql = require('mysql2');
+const dotenv = require('dotenv');
 
-const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'Fatcat14.exe',
-    database: 'community'
+dotenv.config({path: './.env'})
+
+const db = mysql.createConnection({
+    host: process.env.DATABASE_HOST,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE
 });
-
-connection.connect((err) => {
+db.connect((err) => {
     if (err) {
         console.error('not conect:', err);
         return;
     }
     console.log('connecting!');
 });
+
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs')
-app.use(express.static(path.join(__dirname, 'public')))
+app.use(express.urlencoded({extended:false}))
 app.use(router)
+app.use(express.static(path.join(__dirname, 'public')))
 
 app.listen(8080, () => {
     console.log("http://localhost:8080")
