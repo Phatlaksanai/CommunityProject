8b553985d0f2cc83e288f1117935177d6ac9dd70
weerapat-suw <msmark0015@gmail.com>
2026-02-07 14:06:51 +0700

    m

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -217,13 +217,13 @@ function App() {
       ]
     },
     {
-      path: "/descproject",
+      path: "/descproject/:id",
       element: (
         <DescriptionProject />
       ),
       children: [
         {
-          path: ":id",
+          path: true,
           element: <DescProject />
         },
       ]
